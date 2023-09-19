74e92e8b4941660135132ea24ce1e3440642e451
HendrikHuel <81749071+HendrikHuel@users.noreply.github.com>
2023-09-19 09:06:19 +0200

    experiments

---
+++ chapter-3/experiments/chapter_3_1_2_counter_flow.py
@@ -0,0 +1,26 @@
+from metaflow import FlowSpec, step
+
+
+class CounterFlow(FlowSpec):
+    @step
+    def start(self):
+        """Starting point."""
+        self.count = 0
+        self.next(self.add)
+
+    @step
+    def add(self):
+        """Just say hi."""
+        print(f"The count is {self.count} before incrementing.")
+        self.count += 1
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """Finish line."""
+        self.count += 1
+        print(f"The final count is {self.count}.")
+
+
+if __name__ == "__main__":
+    CounterFlow()
